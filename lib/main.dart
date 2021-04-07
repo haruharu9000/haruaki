5d261f6436032668d1c94c013f891e890fc874bb
haruaki <nitori4baime@gmail.com>
2021-04-07 19:57:50 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(TestPage());
+
+class TestPage extends StatefulWidget {
+  @override
+  _TestPageState createState() => _TestPageState();
 }
 
-class MyApp extends StatelessWidget {
-  @override
+class _TestPageState extends State<TestPage> {
+
+  String _payment = '未選択';
+
+  void _handleRadioButton(String payment) => setState(() { _payment = payment; } );
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'hogeApp'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                // 枠線
-                border: Border.all(color: Colors.black, width: 1),
-                // 角丸
-                borderRadius: BorderRadius.circular(8),
-              ),
-              padding: EdgeInsets.only(top: 50, right: 30, bottom: 100, left: 30),
-              margin: EdgeInsets.all(15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('お付き合いをはじめて',
-                    style: TextStyle(
-                    height: 0,
-                    ),
-                  ),
-                  Text('100日目',
-                    style: TextStyle(
-                    height: 2.0,
-                        fontSize: 22,
-                    ),
-                  ),
-                  new Divider(
-                      color: Colors.black
-                  ),
-                  Text('お付き合い記念日  :  2021年1月1日',
-                    style: TextStyle(
-                      height: 3.0,
-                    ),
-                  ),
-                  new Divider(
-                      color: Colors.black
-                  ),
-                  Container(
-                    child: Align(
-                      alignment: Alignment(-0.28, 0),
-                        child : Text('次の記念日まで  :  30日',
-                        style: TextStyle(
-                        height: 3.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  new Divider(
-                      color: Colors.black
-                  ),
-                ],
-              ),
-            ),
-          ],
+      title: '',
+      home: Scaffold(
+        appBar: AppBar(title: Text("ほむほむゆんゆ日記",
+          style: TextStyle(color: HexColor('#696969'),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              backgroundColor: HexColor('#fff3b8'),
+            ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '今、どんな気持ちですか？',
+                style: TextStyle(
+                  fontSize: 20,
+                  ),
+              ),
+          Container(
+
+            decoration: BoxDecoration(
+              // 枠線
+              border: Border.all(color: HexColor('#e6c0c0'), width: 2),
+              // 角丸
+              borderRadius: BorderRadius.circular(8),
+
+            ),
+            margin: EdgeInsets.fromLTRB(15, 15, 15, 15),
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            width: 300.0,
+            height: 300.0,
+            child: TextField(
+              maxLength: 140, //入力最大文字数
+              maxLines:10,
+            ),
+
+            ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  '体調はいかがですか？',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Radio(
+                      activeColor: Colors.blueAccent,
+                      value: 'とても良い',
+                      groupValue: _payment,
+                      onChanged: _handleRadioButton,
+                    ),
+                    Text('とても良い'),
+                  ],
+                ),Column(
+                  children: <Widget>[
+                    Radio(
+                      activeColor: Colors.blueAccent,
+                      value: '良い',
+                      groupValue: _payment,
+                      onChanged: _handleRadioButton,
+                    ),
+                    Text('良い'),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Radio(
+                      activeColor: Colors.blueAccent,
+                      value: '悪い',
+                      groupValue: _payment,
+                      onChanged: _handleRadioButton,
+                    ),
+                    Text('悪い'),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Radio(
+                      activeColor: Colors.blueAccent,
+                      value: 'とても悪い',
+                      groupValue: _payment,
+                      onChanged: _handleRadioButton,
+                      ),
+                    Text('とても悪い'),
+    ],
+                ),
+              ],
+            ),
+            ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: null,
+                child: Text('できた'),
+          ),
+
+            ],
+          ),
+         ],
+        ),
+    ),
     );
   }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
